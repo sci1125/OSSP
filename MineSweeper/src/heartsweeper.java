84140d6492116e15eea570a677be44f0e2cb8687
sci1125 <starchang25@naver.com>
2023-01-18 18:16:28 +0900

    커스텀 추가

--- MineSweeper/src/heartsweeper.java
+++ MineSweeper/src/heartsweeper.java
@@ -16,22 +16,22 @@ interface minesweeper{
 //메인 화면
 class TitlePanel extends JPanel{
     JPanelChange pc;
-    JButton[] btn = new JButton[3];
+    JButton[] btn = new JButton[4];
     public TitlePanel(JPanelChange pc){
         this.pc = pc;
         setLayout(null);
         JLabel lb = new JLabel("지뢰 찾기");
         lb.setFont(new Font("맑은 고딕", Font.PLAIN, 50));
-        lb.setBounds(180, 400, 300, 90);
+        lb.setBounds(270, 400, 320, 90);
         lb.setHorizontalAlignment(SwingConstants.CENTER);
         String path = TitlePanel.class.getResource("").getPath();
         ImageIcon im = new ImageIcon(path+"지뢰 모양"
         		+ ".png");
         JLabel imlb = new JLabel(im);
-        imlb.setBounds(0, 0, 700, 500);
-        String[] st = {"10 x 10", "15 x 15", "20 x 20"};
+        imlb.setBounds(0, 0, 900, 500);
+        String[] st = {"10 x 10", "15 x 15", "20 x 20", "custom"};
         int j=0;
-        for(int i=100;i<600;i+=180){
+        for(int i=100;i<800;i+=200){
             btn[j] = new JButton(st[j]);
             btn[j].setBounds(i, 550, 100, 30);
             btn[j].addActionListener(new MyActionListener());
@@ -40,44 +40,67 @@ class TitlePanel extends JPanel{
         add(lb);
         add(imlb);
     }
-    
     class MyActionListener implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            JButton b = (JButton)e.getSource();
-            if(b.getText().equals("10 x 10"))
-                pc.change("panel10");
-            else if(b.getText().equals("15 x 15"))
-                pc.change("panel15");
-            else
-                pc.change("panel20");
+        	JButton b = (JButton)e.getSource();
+        	if(b.getText().equals("10 x 10")) {
+            	pc.change("panel10");
+            }                
+            else if(b.getText().equals("15 x 15")) {
+            	pc.change("panel15");
+            }                
+            else if (b.getText().equals("20 x 20")) {         
+            	pc.change("panel20");
+            }                
+            else {
+            	// custom grid case 
+            	JTextField xField = new JTextField(5);
+                JTextField yField = new JTextField(5);
+                JPanel myPanel = new JPanel();
+                myPanel.add(new JLabel("가로 :"));
+                myPanel.add(xField);
+                myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+                myPanel.add(new JLabel("세로 :"));
+                myPanel.add(yField);
+                // 가로 세로 규격을 묻는 다이얼로그
+                int result = JOptionPane.showConfirmDialog(null, myPanel, 
+                         "가로 세로 값을 모두 입력해주세요", JOptionPane.OK_CANCEL_OPTION);
+                if (result == JOptionPane.OK_OPTION) {
+                	pc.panelCustom = new PanelCustom(pc, Integer.parseInt(yField.getText()), Integer.parseInt(xField.getText()));                	
+                	pc.change("custom size");
+                }                        
+            }
         }
     }
 }
 
-//10 x 10 지뢰찾기
+// 10 x 10 지뢰찾기
 class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 화면 구성 */
-    JPanelChange pc;
+	JPanelChange pc;
     GridBagLayout grid;
     GridBagConstraints gbc = new GridBagConstraints();
     JButton[][] btn = new JButton[10][10];
     String[][] arr = new String[10][10];
-	public Panel10(JPanelChange pc){
+    private JTextField textFieldPlayTime;
+    
+    public Panel10(JPanelChange pc){   
         grid = new GridBagLayout();
         setLayout(grid);
-        this.pc = pc;
-        setScreen();
-    }
+        this.pc = pc;       
+        setScreen();        
+    }    
+    
     @Override
     public void setScreen(){
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx=1.0;
         gbc.weighty=1.0;
-
         JTextField tx = new JTextField(20);
         tx.setText("9");
         tx.setHorizontalAlignment(JLabel.CENTER);
         tx.setEditable(false);
         createHeart();
+        
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
                 btn[i][j] = new JButton(String.valueOf(arr[i][j]));
@@ -92,7 +115,7 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
         }
         addGrid(tx, 0, 0, 10);
     }
-    //지뢰 생성
+    // 지뢰 생성
     public void createHeart() {
         Random rand = new Random();
         int mine = 9;
@@ -146,11 +169,23 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
         JButton[][] btn;
         JTextField tx;
         int k;
+        int customRow, customCol;
+        
         public bntMouseAdapter(JTextField tx, int k, JButton[][] btn){
             this.tx = tx;
             this.k = k;
+            this.customRow = k;
+            this.customCol = k;
             this.btn = btn;
         }
+        
+        public bntMouseAdapter(JTextField tx, int customRow, int customCol, JButton[][] btn){
+            this.tx = tx;            
+            this.customRow = customRow;
+            this.customCol = customCol;
+            this.btn = btn;
+        }
+        
         public void mouseClicked(MouseEvent e){
             if(e.getButton()==MouseEvent.BUTTON3){
                 JButton b = (JButton)e.getSource();
@@ -160,14 +195,12 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
                     tx.setText(String.valueOf(Integer.parseInt(tx.getText())-1));
                 }
                 int state=1;
-                for(int i=0;i<k;i++){
-                    for(int j=0;j<k;j++){
+                for(int i=0;i<customRow;i++){
+                    for(int j=0;j<customCol;j++){
                         if(btn[i][j].isEnabled())
                            state = 0;
                     }
                 }
-                if(state == 1)
-                    JOptionPane.showMessageDialog(null, "이겼습니다!", "안내", JOptionPane.INFORMATION_MESSAGE);
             }
         }
     }
@@ -175,6 +208,8 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
         String[][] arr;
         JButton[][] btn;
         int row, col, k;
+        private JPanel currentPanel;
+        
         public bntActionListener(JButton[][] btn, String[][] arr, int row, int col, int k){
             this.btn = btn;
             this.arr = arr;
@@ -182,7 +217,12 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
             this.col = col;
             this.k = k;
         }
-        public void findAction(int row, int col, int k){
+        
+        public void setCurrentPanel(JPanel panel) {
+        	currentPanel = panel;
+        }
+        
+        public void findAction	(int row, int col, int k){
             if(row<0||row>=k||col<0||col>=k||arr[row][col].equals("-2")||arr[row][col].equals("-1"))
                 return;
             if(arr[row][col].equals("0")){
@@ -224,9 +264,19 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
             JButton b = (JButton)e.getSource();
             b.setEnabled(false);
             if(e.getActionCommand().equals("-1")){
-                setIcon(b, "지뢰.png");
-                int result = JOptionPane.showConfirmDialog(null, "다시하시겠습니까?", "안내", JOptionPane.YES_NO_OPTION);
-                if(result!=0) System.exit(0);
+            	setIcon(b, "지뢰.png");          
+                 
+            	// 싱글톤 클래스로부터 저장되어있던 시간 값 가져와서 다이얼로그에 표시
+            	int result = JOptionPane.showConfirmDialog(null, "다시하시겠습니까?", "안내", JOptionPane.YES_NO_OPTION);
+            	if(result!=0)  {
+            		System.exit(0);
+            	}                	
+            	else {
+            		JComponent comp = (JComponent) e.getSource();
+                 	Window win = SwingUtilities.getWindowAncestor(comp);
+                 	win.dispose();
+                 	new heartsweeper();
+            	}
             }
             else if(e.getActionCommand().equals("0")) findAction(row, col, k);
             else{
@@ -248,8 +298,12 @@ class Panel10 extends JPanel implements minesweeper{   /* 각각 지뢰찾기 �
                         state = 0;
                 }
             }
-            if(state == 1)
-                JOptionPane.showMessageDialog(null, "이겼습니다!", "안내", JOptionPane.INFORMATION_MESSAGE);
+            if(state == 1) {
+            	JComponent comp = (JComponent) e.getSource();
+          	    Window win = SwingUtilities.getWindowAncestor(comp);
+          	    win.dispose();
+                new heartsweeper();
+            }
         }
     }
 }
@@ -261,12 +315,14 @@ class Panel15 extends JPanel implements minesweeper{
     GridBagConstraints gbc = new GridBagConstraints();
     JButton[][] btn = new JButton[15][15];
     String[][] arr = new String[15][15];
+    
     public Panel15(JPanelChange pc){
         grid = new GridBagLayout();
         setLayout(grid);
         this.pc = pc;
         setScreen();
     }
+    
     @Override
     public void setScreen(){
         gbc.fill = GridBagConstraints.BOTH;
@@ -344,13 +400,14 @@ class Panel15 extends JPanel implements minesweeper{
     }
 }
 
-// 20 x 20 지뢰찾기
+// 20 x 20 지뢰 찾기
 class Panel20 extends JPanel implements minesweeper{
     JPanelChange pc;
     GridBagLayout grid;
     GridBagConstraints gbc = new GridBagConstraints();
     JButton[][] btn = new JButton[20][20];
     String[][] arr = new String[20][20];
+    
     public Panel20(JPanelChange pc){
         grid = new GridBagLayout();
         setLayout(grid);
@@ -433,23 +490,130 @@ class Panel20 extends JPanel implements minesweeper{
     }
 }
 
-//게임판 셍성
+//custom size 로 지뢰찾기 그리드를 만드는 패널 클래스
+class PanelCustom extends JPanel implements minesweeper{
+    JPanelChange pc;
+    GridBagLayout grid;
+    GridBagConstraints gbc = new GridBagConstraints();
+    JButton[][] btn;
+    String[][] arr;
+    int customRow = 0; // 가로 규격 Row
+    int customCol = 0; // 세로 규격 Column
+    JTextField textFieldPlayTime;
+    
+    public PanelCustom(JPanelChange pc, int row, int col){
+        grid = new GridBagLayout();
+        setLayout(grid);
+        this.pc = pc;           
+        customRow = row;
+        customCol = col;
+        btn = new JButton[customRow][customCol];
+        arr = new String[customRow][customCol];      
+        setScreen();
+    }
+    
+    @Override
+    public void setScreen(){
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx=1.0;
+        gbc.weighty=1.0;
+        JTextField tx = new JTextField(20);
+        tx.setText(String.valueOf((customRow * customCol) / 9));
+        tx.setHorizontalAlignment(JLabel.CENTER);
+        tx.setEditable(false);
+        createHeart();                         
+        
+        for(int i=0;i<customRow;i++){
+            for(int j=0;j<customCol;j++){
+                btn[i][j] = new JButton(String.valueOf(arr[i][j]));
+                btn[i][j].setBackground(Color.PINK);
+                btn[i][j].setForeground(Color.PINK);
+                btn[i][j].setPreferredSize(new Dimension(10, 10));
+                Panel10.bntActionListener bn = new Panel10.bntActionListener(btn, arr, i, j, customRow);
+                btn[i][j].addActionListener(bn);
+                btn[i][j].addMouseListener(new Panel10.bntMouseAdapter(tx, customRow, customCol, btn));
+                addGrid(btn[i][j], j, i+1, 1);
+            }
+        }
+        addGrid(tx, 0, 0, 20);             
+    }
+    
+    public void createHeart() {
+        Random rand = new Random();
+        // custom 규격의 경우 mine의 적당한 수를 위해계산식을 세움.
+        int mine = (customRow * customCol) / 9;
+        for(int i=0;i<customRow;i++){
+            for(int j=0;j<customCol;j++)
+                arr[i][j]="0";
+        }
+        while (mine-- > 0) {
+            int row = rand.nextInt(customRow);
+            int col = rand.nextInt(customCol);
+            if (arr[row][col].equals("-1"))
+                mine++;
+            if (arr[row][col].equals("0"))
+                arr[row][col] = "-1";
+        }
+        for(int i=0;i<customRow;i++){
+            for(int j=0;j<customCol;j++){
+                int a = getMine(i, j);
+                if(arr[i][j].equals("0")&&a!=0)
+                    arr[i][j] = String.valueOf(a);
+            }
+        }
+    }
+    public boolean isExist(int row, int col){
+        if(row<0||row>=customRow||col<0||col>=customCol)
+            return false;
+        return arr[row][col].equals("-1");
+    }
+    public int getMine(int row, int col){
+        int cnt = 0;
+        if(isExist(row-1, col-1)) cnt++;
+        if(isExist(row-1, col)) cnt++;
+        if(isExist(row-1, col+1)) cnt++;
+        if(isExist(row, col-1)) cnt++;
+        if(isExist(row, col+1)) cnt++;
+        if(isExist(row+1, col-1)) cnt++;
+        if(isExist(row+1, col)) cnt++;
+        if(isExist(row+1, col+1)) cnt++;
+        return cnt;
+    }
+    public void addGrid(Component c, int x, int y, int w){
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = 1;
+        grid.setConstraints(c, gbc);
+        add(c);
+    }
+}
+
 class JPanelChange extends JFrame{
     TitlePanel titlePanel = null;
     Panel10 panel10 = null;
     Panel15 panel15 = null;
     Panel20 panel20 = null;
-
+    PanelCustom panelCustom = null;
+    
     public void change(String panelName){
         getContentPane().removeAll();
         switch (panelName) {
-            case "titlePanel":
-                getContentPane().add(titlePanel);break;
-            case "panel10":
-                getContentPane().add(panel10);break;
-            case "panel15":
-                getContentPane().add(panel15);break;
-            default: getContentPane().add(panel20);break;
+        case "titlePanel":            	
+            getContentPane().add(titlePanel);               
+            break;  
+        case "panel10":            
+            getContentPane().add(panel10);              
+            break;    
+        case "panel15":            	
+            getContentPane().add(panel15);                
+            break; 
+        case "panel20":            
+        	getContentPane().add(panel20);            
+        	break;
+        default: 
+        	getContentPane().add(panelCustom);            	
+        	break;
         }
         revalidate();
         repaint();
@@ -469,16 +633,16 @@ public class heartsweeper extends JFrame {
         createMenu();
         panelSet.add(panelSet.titlePanel);
         panelSet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panelSet.setSize(700, 700);
+        panelSet.setSize(900, 700);
         panelSet.setVisible(true);
     }
 
     public void createMenu(){   // 메뉴 바 만드는 함수
         JMenuBar mb = new JMenuBar();
         String[] barName = {"Game", "Help"};
-        String[][] subItem = {{"new game", "10 x 10", "15 x 15", "20 x 20"}, {"도움말"}};
-        JMenu[] menu = new JMenu[3];
-        JMenuItem[][] menuItems = new JMenuItem[3][];
+        String[][] subItem = {{"Home", "10 x 10", "15 x 15", "20 x 20", "custom"}, {"도움말"}};
+        JMenu[] menu = new JMenu[4];
+        JMenuItem[][] menuItems = new JMenuItem[4][];
         for(int i=0;i<barName.length;i++){
             menu[i] = new JMenu(barName[i]);
             menu[i].setFont(new Font("맑은 고딕", Font.ITALIC, 13));
@@ -508,6 +672,7 @@ public class heartsweeper extends JFrame {
                 case "15 x 15": panelSet.change("panel15");break;
                 case "20 x 20": panelSet.change("panel20");break;
                 default:
+                	//도움말
                     JTextArea textArea = new JTextArea(6, 25);
                     String path = TitlePanel.class.getResource("").getPath();
                     StringBuilder line = new StringBuilder();
